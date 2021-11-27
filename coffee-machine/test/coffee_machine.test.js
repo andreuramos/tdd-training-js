b6a3e87d6538cebc0d2232308cd74b62529ea0b1
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2021-11-27 18:07:16 +0100

    refactor - extract collab & instance

--- coffee-machine/test/coffee_machine.test.js
+++ coffee-machine/test/coffee_machine.test.js
@@ -2,31 +2,24 @@ let CoffeeMachine = require('../src/coffee_machine');
 let DrinkMaker = require('../src/drink_maker');
 
 describe('CoffeeMachine', function() {
+  let drinkMaker = {
+    execute: jest.fn(),
+  }
+  let coffeeMachine = new CoffeeMachine(drinkMaker);
+
   it('makes coffe when coffee button is pressed', function() {
-    let drinkMaker = {
-      execute: jest.fn(),
-    }
-    let coffeeMachine = new CoffeeMachine(drinkMaker);
     coffeeMachine.pressCoffee()
 
     expect(drinkMaker.execute).toHaveBeenCalledWith("C::");
   });
 
   it('makes tea when tea button is pressed', function() {
-    let drinkMaker = {
-      execute: jest.fn(),
-    }
-    let coffeeMachine = new CoffeeMachine(drinkMaker);
     coffeeMachine.pressTea()
 
     expect(drinkMaker.execute).toHaveBeenCalledWith("T::");
   })
 
   it('makes chocolate when chocolate button is pressed', function() {
-    let drinkMaker = {
-      execute: jest.fn(),
-    }
-    let coffeeMachine = new CoffeeMachine(drinkMaker);
     coffeeMachine.pressChocolate()
 
     expect(drinkMaker.execute).toHaveBeenCalledWith("H::");
